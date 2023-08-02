e3cefe6a5f549e215145518ea7e0e211960778f0
dimdimuzun <39340660+dimdimuzun@users.noreply.github.com>
2023-08-02 08:57:08 +0300

    Update tf_aws_modules_check.sh

--- solution/tf_aws_modules_check.sh
+++ solution/tf_aws_modules_check.sh
@@ -3,7 +3,7 @@
 exit_code=1
 
 # Two insights present in the following conditional statement:
-# "arn:aws:ec2:eu-central-1" - means, that real resource created in AWS
+# "arn:aws:ec2:eu-central-1" - means, that real resource already created in AWS
 # "-eq 8" - not clear, because we looked for 7 substrings. That's because in
 # "security_group_ids" output presented string with "vpc_id"
 # To be shure you could run only "grep" part and you will see it.
