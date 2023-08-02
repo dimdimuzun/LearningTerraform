f4771e12cf2461ceecd83d7bc9e0fa38886046d1
dimdimuzun <39340660+dimdimuzun@users.noreply.github.com>
2023-08-02 08:43:12 +0300

    Update tf_aws_modules_check.sh

--- solution/tf_aws_modules_check.sh
+++ solution/tf_aws_modules_check.sh
@@ -8,7 +8,7 @@ exit_code=1
 # "security_group_ids" output presented string with "vpc_id"
 # To be shure you could run only "grep" part and you will see it.
 
-if [ $(grep -E -w 'ec2_ami|ec2_public_ip|ec2_type|arn:aws:ec2:eu-central-1|vpc_id|subnet_id|security_group_ids' result | wc -l) -eq 8 ]; then
+if [ $(grep -E -w 'ec2_ami|ec2_public_ip|ec2_type|arn:aws:ec2:eu-central-1|vpc_id|subnet_id|security_group_ids' result_example | wc -l) -eq 8 ]; then
    exit_code=0
 fi
 
